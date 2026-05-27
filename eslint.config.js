4c719e2e5510f1c37dfed53c3d72f9dc2181d479
LuispeFigueroa <fig24087@uvg.edu.gt>
2026-05-26 23:19:14 -0600

    configurar ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,18 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default [
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
+    files: ['src/**/*.{js,jsx,ts,tsx}'],
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
     languageOptions: {
-      globals: globals.browser,
+      parser: tsParser,
+    },
+    rules: {
+      semi: ['error', 'never'],
+      'max-len': ['error', { code: 120 }],
     },
   },
-])
+]
